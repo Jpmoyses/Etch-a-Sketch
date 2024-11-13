a33c14485d266caf0e1a50853407b7de64980628
jpmoyses <jpmoyses00@gmail.com>
2024-11-13 15:45:53 -0300

    Added color options and new font

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const container = document.querySelector(".container");
 
+
+const colorButtons = document.querySelectorAll(".colorPicker");
+let colorSelected = 1;
+
+colorButtons.forEach((button) =>{
+    button.addEventListener("click", () =>{
+        updateButton();
+        if (button.id == "color1"){
+            colorSelected = 1;
+            button.classList.add("selected");
+        }
+        else if (button.id == "color2"){
+            colorSelected = 2;
+            button.classList.add("selected");
+        }
+        else if (button.id == "color3"){
+            colorSelected = 3;
+            button.classList.add("selected");
+        }
+    })
+})
+
+function updateButton(){
+    colorButtons.forEach((button) => {
+        button.classList.remove("selected");
+    })
+}
+
+
+
 for (let i = 0; i < 16; i++){
     const row = document.createElement("div");
     row.classList.add("row");
@@ -7,9 +37,16 @@ for (let i = 0; i < 16; i++){
         const square = document.createElement("div");
         square.classList.add("square");
         square.addEventListener("mouseover", () =>{
-            square.classList.add("bgColor");
+            removePreviousColor(square);
+            if(colorSelected == 1){square.classList.add("color");}
+            if(colorSelected == 2){square.classList.add("color2");}
+            if(colorSelected == 3){square.classList.add("color3");}
         })
         row.appendChild(square);
     }
     container.appendChild(row);
+}
+
+function removePreviousColor(square){
+    square.classList.remove("color", "color2", "color3");
 }
