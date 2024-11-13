fcf5c851b1224890a426b57b3056c77418bd2c28
jpmoyses <jpmoyses00@gmail.com>
2024-11-13 17:04:39 -0300

    Finished working on the website

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Etch-a-Sketch
+Etch-a-Sketch is a website where you can draw on a small canvas with 3 different colors.
+
+This project made using HTML, CSS and JavaScript as a learning exercise to practice interactions between Flexbox and DOM.
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const container = document.querySelector(".container");
 
-
+// changes in the color selected
 const colorButtons = document.querySelectorAll(".colorPicker");
 let colorSelected = 1;
-
 colorButtons.forEach((button) =>{
     button.addEventListener("click", () =>{
         updateButton();
@@ -21,32 +20,66 @@ colorButtons.forEach((button) =>{
         }
     })
 })
-
+// updates the button pressed
 function updateButton(){
     colorButtons.forEach((button) => {
         button.classList.remove("selected");
     })
 }
 
+// getting the square size
+let x = 16;
+let containerWidth = container.offsetWidth;
+let containerHeight = container.offsetHeight;
 
-
-for (let i = 0; i < 16; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", () =>{
-            removePreviousColor(square);
-            if(colorSelected == 1){square.classList.add("color");}
-            if(colorSelected == 2){square.classList.add("color2");}
-            if(colorSelected == 3){square.classList.add("color3");}
-        })
-        row.appendChild(square);
-    }
-    container.appendChild(row);
+function setSquareSize(){
+    const squareSize = Math.min(containerWidth, containerHeight) / x;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+    });
 }
 
+// initial build of the grid
+function createGrid(){
+    if (x == null){
+        return;
+    }
+    container.innerHTML = '';
+    for (let i = 0; i < x; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < x; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", () =>{
+                removePreviousColor(square);
+                if(colorSelected == 1){square.classList.add("color");}
+                if(colorSelected == 2){square.classList.add("color2");}
+                if(colorSelected == 3){square.classList.add("color3");}
+            })
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+    setSquareSize();
+}
+
+// removes all color before adding a new one to the square
 function removePreviousColor(square){
     square.classList.remove("color", "color2", "color3");
 }
+
+// listener of the change grid button
+const changeGrid = document.querySelector(".btn");
+changeGrid.addEventListener("click", () =>{
+    let size = null;
+    while (size < 2 || size > 100 || isNaN(size)==! ""){
+        size = prompt("Choose a new number of square per side(maximum of 100):");
+    }
+    x = size;
+    createGrid();
+})
+
+createGrid();
